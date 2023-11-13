63819d33d8f34851a8af2105a815457ed53ad5da
unknown <you@example.com>
2023-11-13 11:01:50 -0300

    mcarolinadiaz - linkedlist custom was created with generic properties . it was implemented with NodeGeneric private class

--- src/Transport/LinkedList.java
+++ src/Transport/LinkedList.java
@@ -1,20 +1,58 @@
 package Transport;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LinkedList<T>{
     private final T propertie;
-    private final Set<NodeGeneric<T>> nodeGroup = new HashSet<NodeGeneric<T>>();
+    private NodeGeneric<T> node = null;
     private static class NodeGeneric<T> {
         private T propertieNode;
         private int position;
         private NodeGeneric<T> following;
+        public NodeGeneric() {
+            this.position = 0;
+            this.following = null;
+        }
+
+        public NodeGeneric(NodeGeneric<T> node) {
+            this.propertieNode = node.getPropertieNode();
+            this.following = node.getFollowing();
+            this.position = node.getPosition();
+        }
+
+        public NodeGeneric(T propertieNode, int position, NodeGeneric<T> following) {
+            this.propertieNode = propertieNode;
+            this.following = new NodeGeneric<T>(following);
+            this.position = position;
+        }
+
+        public NodeGeneric(T propertieNode, int position) {
+            this.propertieNode = propertieNode;
+            this.position = position;
+            this.following = null;
+        }
+
+        public NodeGeneric(T propertieNode) {
+            this.propertieNode = propertieNode;
+            this.position = 0;
+            this.following = null;
+        }
+
         public void linkedNode(NodeGeneric<T> following){
-            this.following = following;
+            this.following = new NodeGeneric<T>(following);
         }
         public NodeGeneric<T> getFollowing() {
-            return this.following;
+            return new NodeGeneric<T>(this.following);
+        }
+        public void setPosition(int position) {
+            this.position = position;
+        }
+        public int getPosition() {
+            return this.position;
+        }
+        public void setPropertieNode(T propertieNode) {
+            this.propertieNode = propertieNode;
+        }
+        public T getPropertieNode() {
+            return this.propertieNode;
         }
     }
     // Constructor
@@ -22,11 +60,47 @@ public class LinkedList<T>{
         this.propertie = propertie;
     }
 
-    public void add(T element) {/*
+    public void add(T element) {
         if (element.getClass() == this.propertie.getClass()) {
+            NodeGeneric<T> newNode = new NodeGeneric<T>(element);
+            if (this.node == null) {
+                this.node = newNode;
+                this.node.setPosition(0);
+            } else {
+                NodeGeneric<T> indexNode =  new NodeGeneric<T>(this.node);
+                while (indexNode.getFollowing() != null) {
+                    indexNode = indexNode.getFollowing();
+                }
+                newNode.setPosition(indexNode.getPosition() + 1);
+                indexNode.linkedNode(newNode);
+            }
+        }
+    }
 
-            this.nodeGroup.add((NodeGeneric<T>) element);
-        }*/
+    public void add(T element, int position) {
+        if (element.getClass() == this.propertie.getClass()) {
+            NodeGeneric<T> newNode = new NodeGeneric<T>(element);
+            if (this.node == null) {
+                this.node = newNode;
+            } else {
+                NodeGeneric<T> nodeIndex = new NodeGeneric<T>(this.node);
+                while (nodeIndex.getFollowing() != null && nodeIndex.getFollowing().getPosition() < position) {
+                    nodeIndex = nodeIndex.getFollowing();
+                }
+                newNode.setPosition(position);
+                if (nodeIndex.getFollowing() != null) {
+                    newNode.linkedNode(nodeIndex.getFollowing());
+                }
+                nodeIndex.linkedNode(newNode);
+            }
 
+        }
+
+    }
+    public T getPropertie() {
+        return this.propertie;
+    }
+    public boolean isEmptly() {
+        return this.node == null;
     }
 }
